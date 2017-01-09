7e9a4fc594f1b8022e846b59e45e8a2d14c7ef2e
letulip <ivladimirskiy@ya.ru>
2017-01-09 18:09:50 +0300

    Получение пути к файлу и чтение текстового файла добавлено

--- Program.cs
+++ Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace RegexSearch
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите путь к файлу, пожалуйста: ");
+            string filePath = Console.ReadLine();
+
+            string textPattern = @"\.txt";
+            string xlsxPattern = @"\.xlsx";
+            
+            
+            
+
+            try
+            {   
+                using (StreamReader sr = new StreamReader(filePath))
+                {                    
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+
+                Console.ReadKey();
+            }
         }
     }
 }
