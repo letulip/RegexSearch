21537d834832f06442fd57a723e44bab01383cbb
letulip <ivladimirskiy@ya.ru>
2017-01-10 10:54:18 +0300

    RegexChecking функция в отдельном классе

---
+++ RegexChecking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexSearch
+{
+    public static class RegexChecking
+    {        
+        public static int RegexCheck(string[] input)
+        {
+            if (input == null)
+            {
+                return 0;
+            }
+
+            foreach (string str in input)
+            {
+                if (Regex.IsMatch(str, Program.regexPattern))
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
